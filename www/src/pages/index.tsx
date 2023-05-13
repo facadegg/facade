d5690f6c1664a4f1f90f115e3cd8e838fdc3d414
Shukant Pal <SukantK2002@outlook.com>
2023-05-13 15:06:33 -0400

    Change tab title & icon

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -27,4 +27,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Facade − A way to reimagine reality</title>
