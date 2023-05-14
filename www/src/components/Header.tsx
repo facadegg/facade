cd845cb88533d5040dab5a46dd43a84bbb5274a0
Shukant Pal <SukantK2002@outlook.com>
2023-05-13 22:07:57 -0400

    Don't restrict backdrop to less than full width of page in header

--- www/src/components/Header.tsx
+++ www/src/components/Header.tsx
@@ -5,19 +5,28 @@ const headerGapStyle: React.CSSProperties = {
     height: "calc(42px + 5rem)",
 }
 
-const HeaderLayout = styled.div`
+const HeaderBackdrop = styled.div`
   background: rgba(0, 0, 0, .5);
-  border-bottom: 0.5px solid rgba(255, 255, 255, 0.12); 
   backdrop-filter: blur(48px);
   display: flex;
-  gap: 14px;
-  margin: 0 21px 0 21px;
-  padding: 16px 0 16px 0;
+  justify-content: center;
+  padding: 0;
   position: fixed;
-  width: calc(100% - 42px);
+  width: 100%;
   z-index: 1;
 
   -webkit-backdrop-filter: blur(48px);
+`
+
+const HeaderLayout = styled.div`
+  background: rgba(0, 0, 0, 0);
+  border-bottom: 0.5px solid rgba(255, 255, 255, 0.12); 
+  display: flex;
+  gap: 14px;
+  margin: 0 48px 0 48px;
+  max-width: 902px;
+  padding: 16px 0 16px 0;
+  width: calc(100% - 96px);
 `;
 
 const SiteIcon: React.FC<{}> = React.memo(() => {
@@ -33,10 +42,12 @@ const SiteIcon: React.FC<{}> = React.memo(() => {
 const Header: React.FC<{}> = () => {
     return (
         <>
-            <HeaderLayout>
-                <SiteIcon />
-                <span>Facade</span>
-            </HeaderLayout>
+            <HeaderBackdrop>
+                <HeaderLayout>
+                    <SiteIcon />
+                    <span>Facade</span>
+                </HeaderLayout>
+            </HeaderBackdrop>
             <div style={headerGapStyle} />
         </>
     )
